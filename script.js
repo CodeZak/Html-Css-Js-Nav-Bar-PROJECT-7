0daa6ee45a0b28e72d75c2600ba09a37605dc996
Zakariae Mahi <105796338+Zakitek@users.noreply.github.com>
2022-06-02 22:52:43 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const moreBtn = document.querySelector(".more");
+const mobileMenu = document.querySelector(".menu");
+
+moreBtn.addEventListener("click", function () {
+    mobileMenu.classList.toggle("mobile_menu");
+});
